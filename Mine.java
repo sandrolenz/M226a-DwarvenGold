3f17ff4a747147252c19394fc4d5c3786c65afc8
Sandro Lenz <sandro.lenz@edu.tbz.ch>
2022-10-17 10:07:46 +0200

    create greenfoot project

---
+++ Mine.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Mine here.
+ * 
+ * @author Sandro Lenz, Daniel Fankhauser 
+ * @version 0.1
+ */
+public class Mine extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public Mine()
+    {    
+        // Create a new world with 1000x600 cells with a cell size of 1x1 pixels.
+        super(1000, 600, 1); 
+    }
+}
